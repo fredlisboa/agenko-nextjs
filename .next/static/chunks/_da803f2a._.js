3d44a29ddf7df43186418b4f7d22043ae6c5f793
frederiquelisboa <frederiquelisboa@gmail.com>
2025-09-18 17:56:54 +0000

    105

--- .next/static/chunks/_da803f2a._.js
+++ .next/static/chunks/_da803f2a._.js
@@ -610,7 +610,7 @@ const Contact = ()=>{
                                             children: [
                                                 /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("span", {
                                                     className: "sub-title",
-                                                    children: "Agende Sua Avaliação"
+                                                    children: "Agende Sua Avaliação Gratuita"
                                                 }, void 0, false, {
                                                     fileName: "[project]/src/app/Components/Contact/Contact.tsx",
                                                     lineNumber: 307,
