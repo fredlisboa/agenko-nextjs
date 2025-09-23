66ea91bca46af56ca1cf666914ed7dc3f8ebf60d
frederiquelisboa <frederiquelisboa@gmail.com>
2025-09-23 19:34:31 +0000

    137

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -15,5 +15,23 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 0.8,
     },
+    {
+      url: 'https://hof.studiodental.dental/contact',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://hof.studiodental.dental/politica-privacidade',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.3,
+    },
+    {
+      url: 'https://hof.studiodental.dental/termos-de-uso',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.3,
+    },
   ]
 }
