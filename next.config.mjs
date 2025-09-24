d37e6b2857d67e3ee073c0e3e500b18621f69101
frederiquelisboa <frederiquelisboa@gmail.com>
2025-09-24 15:04:36 +0000

    151

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    unoptimized: false,
-  },
-  // Enable SWC Minification for improved performance
-  swcMinify: true,
-  // Configure compiler for styled-components if used
-  compiler: {
-    styledComponents: true,
-    // Remove "use strict" declarations from the output
-    removeStrict: true,
-  },
-  // Add caching headers for static assets
-  async headers() {
-    return [
-      {
-        source: '/:all*(svg|jpg|jpeg|png|webp|gif|ico|css|js)',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
