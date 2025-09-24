e843c49f4079c0f3aea041559be6003ba8eb9c11
frederiquelisboa <frederiquelisboa@gmail.com>
2025-09-24 19:43:02 +0000

    157

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -121,10 +121,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="preconnect" href="https://cdn.jsdelivr.net" />
         <link rel="preconnect" href="https://cdnjs.cloudflare.com" />
 
-        {/* Preload fonts */}
-        <link rel="preload" href="https://fonts.bunny.net/fonts/ubuntu-latin-400-normal.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
-        <link rel="preload" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
-
         {/* Preload all stylesheets to ensure they are fetched early */}
         <link rel="preload" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" as="style" />
         <link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css" as="style" />
@@ -191,7 +187,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={`${source_sans.variable} ${roboto.variable}`}>
         <UTMProvider>
           {/* Google Tag Manager (noscript) */}
-          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MGLJVNRZ"
+          <noscript><iframe src="https://www.googletagManager.com/ns.html?id=GTM-MGLJVNRZ"
           height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe></noscript>
           {/* End Google Tag Manager (noscript) */}
           {children}
