eafb94248f8fc1529672dec511d392f67531e5fc
frederiquelisboa <frederiquelisboa@gmail.com>
2025-09-25 11:27:02 +0000

    ci: Untrack .next build folder

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"iJIn3HIHBk4m7kLANxrAdZCdJybdgR/9uHsirwv0SZM=\"\n}"
--- .next/static/chunks/src_app_layout_tsx_0a548d63._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_2e060c72._.js",
-  "static/chunks/[root-of-the-server]__54d9be7e._.css"
-],
-    source: "dynamic"
-});
